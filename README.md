17e64592ab4cc635f389c96c1a78cbd277047796
olivier delfosse <olividelfosse@gmail.com>
2020-08-14 15:50:02 +0200

    Add dotenv - Protect my secret data in .env file

--- README.md
+++ README.md
@@ -20,5 +20,14 @@ Things you may want to cover:
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
+>heroku create $YOUR_APP_NAME --region eu
 
-* ...
+!!!! USE root in your routes and views ! or you don't find your app at
+https://cocktails-435-1.herokuapp.com/
+
+
+maj db:
+git push heroku master
+
+heroku run rails db:migrate
+heroku run rails db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,15 @@ Dose.destroy_all
 puts "destroy_all"
 Ingredient.destroy_all
 
+IMAGE_URLS = [
+  "https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1125&q=80",
+  "https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1536599424071-0b215a388ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1556855810-ac404aa91e85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+]
+
 # db/seeds.rb
 puts "create 3 Ingredients"
 Ingredient.create(name: "lemon")
@@ -24,12 +33,12 @@ require 'open-uri'
 source = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 data = JSON.load(open(source))
 
-Cocktail.create(name: "cocktail banane")
-Cocktail.create(name: "cocktail ice")
-Cocktail.create(name: "cocktail mint leaves")
-Cocktail.create(name: "cocktail banane 2")
-Cocktail.create(name: "cocktail ice 2")
-Cocktail.create(name: "cocktail mint leaves 2")
+Cocktail.create(name: "cocktail banane", image_url: IMAGE_URLS.sample())
+Cocktail.create(name: "cocktail ice", image_url: IMAGE_URLS.sample())
+Cocktail.create(name: "cocktail mint leaves", image_url: IMAGE_URLS.sample())
+Cocktail.create(name: "cocktail banane 2", image_url: IMAGE_URLS.sample())
+Cocktail.create(name: "cocktail ice 2", image_url: IMAGE_URLS.sample())
+Cocktail.create(name: "cocktail mint leaves 2", image_url: IMAGE_URLS.sample())
 puts "cocktails create 6"
 
 data["drinks"].sample(10).each do |ingredient|
